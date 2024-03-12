fad063cf16272e51a9e00e44687d2a7919aecfad
Luiz Manoel <dantasluizmanoel@gmail.com>
2024-03-11 23:15:59 -0300

    pratica objetos

--- classes-objetos/src/main/kotlin/br/edu/ifpb/pdm/classesobjetos/ClassesObjetosApplication.kt
+++ classes-objetos/src/main/kotlin/br/edu/ifpb/pdm/classesobjetos/ClassesObjetosApplication.kt
@@ -10,33 +10,31 @@ package br.edu.ifpb.pdm.classesobjetos
  * - Nome (string): o nome do funcionário.
  * - Idade (int): a idade do funcionário.
  */
-open class Funcionario( nome: String, idade: Int){
-
+open class Funcionario( var nome: String, var idade: Int){
+    init{
+        println("Um novo funcionario foi adicionado -> Nome: $nome - Idade: $idade")
+    }
+    open fun apresentacao(){
+        println("Olá! Me chamo $nome e tenho $idade anos!")
+    }
+}
+class Gerente(nome: String, idade: Int, var setor: String) :Funcionario(nome, idade){
+    override fun apresentacao(){
+        println("Olá! Me chamo $nome e tenho $idade anos! Sou do setor $setor")
+    }
+}
+class Desenvolvedor(nome: String, idade: Int, var linguagem: String) :Funcionario(nome, idade){
+    override fun apresentacao(){
+        println("Olá! Me chamo $nome e tenho $idade anos! Sou desenvolvedor $linguagem")
+    }
+}
+class Analista(nome: String, idade: Int, var area: String) :Funcionario(nome, idade){
+    override fun apresentacao(){
+        println("Olá! Me chamo $nome e tenho $idade anos! Sou Analista $area")
+    }
 }
 /**
- *
- * 2.Crie uma classe Gerente que herda da classe Funcionario e adicione uma propriedade adicional:
- * - Setor (string): o setor em que o gerente trabalha.
- */
-class Gerente(nome: String, idade: Int, setor: String) :Funcionario(nome, idade){
-
-}
-/**
- * 3.Crie uma classe Desenvolvedor que herda da classe Funcionario e adicione uma propriedade adicional:
- * - Linguagem (string): a linguagem de programação que o desenvolvedor utiliza.
- */
-class Desenvolvedor(nome: String, idade: Int, linguagem: String) :Funcionario(nome, idade){
-
-}
-/**
- * 4.Crie uma classe Analista que herda da classe Funcionario e adicione uma propriedade adicional:
- * - Area (string): a área de especialização do analista.
- */
-class Analista(nome: String, idade: Int, area: String) :Funcionario(nome, idade){
-
-}
-/**
- * 5.Imprima uma mensagem informando que um novo funcionário foi registrado, juntamente com o nome e a idade do
+ * 5. Imprima uma mensagem informando que um novo funcionário foi registrado, juntamente com o nome e a idade do
  * funcionário. (Lembre-se do init)
  */
 
@@ -47,6 +45,20 @@ class Analista(nome: String, idade: Int, area: String) :Funcionario(nome, idade)
  * 7. Crie uma lista de funcionários e adicione diferentes tipos de funcionários (gerentes, desenvolvedores e analistas)
  * à lista.
  *
+ *
+ *
+ *
  * 8. Utilize o typecast (is e as) para verificar o tipo de cada funcionário na lista e chamar o método Apresentar
- * correspondente.
- */
+ * correspondente.*/
+
+fun main(){
+    val listaFuncionarios = listOf(
+        Funcionario("Andre", 23),
+        Gerente("Tomas", 27, "RH"),
+        Desenvolvedor("Caio", 19, "Kotlin"),
+        Analista("Carla", 37, "Analise de Dados")
+    )
+    for ( funcionario in listaFuncionarios){
+        funcionario.apresentacao()
+    }
+}
